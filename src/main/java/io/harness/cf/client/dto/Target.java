4452123549d5b3928ae8c288131c5c79c228358d
Rushabh Shah <rushabh.shah@harness.io>
2021-04-08 19:48:20 -0700

    [FFM-732]: fix jws dependency issue and nullPointerException for AnalyticsPublisher

--- src/main/java/io/harness/cf/client/dto/Target.java
+++ src/main/java/io/harness/cf/client/dto/Target.java
@@ -1,9 +1,11 @@
 package io.harness.cf.client.dto;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +20,7 @@ import lombok.Setter;
 public class Target {
   private String identifier;
   private String name;
-  private Map<String, Object> attributes;
+  @Default private Map<String, Object> attributes = new HashMap<>();
   private boolean isPrivate; // If the target is private
   private Set<String> privateAttributes; // Custom set to set the attributes which are private
 
