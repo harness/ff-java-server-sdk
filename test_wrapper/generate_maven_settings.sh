4432a739f8cf580ad2a9ec96944d4b7a95af8718
Milos Vasic <milos.vasic@harness.io>
2021-08-09 17:04:11 +0200

    FFM-1001 - Cleaning up.

--- test_wrapper/generate_maven_settings.sh
+++ test_wrapper/generate_maven_settings.sh
@@ -5,10 +5,11 @@ SETTINGS="$MVN_HOME/settings.xml"
 
 mkdir -p "$MVN_HOME" && touch "$SETTINGS" &&
 cat >> "$SETTINGS"<< EOF
-echo """<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
-          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-          xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
-                          https://maven.apache.org/xsd/settings-1.0.0.xsd">
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
+                    https://maven.apache.org/xsd/settings-1.0.0.xsd">
+
     <servers>
 
         <server>
