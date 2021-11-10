3916aec4e4e04ba27c3567982b5f6434a2fa7e7e
Milos Vasic <milos.vasic@harness.io>
2021-11-10 14:00:42 +0300

    FFM-1543 - Optimize imports.

--- src/test/java/io/harness/cf/client/api/evaluator/EvaluatorTest.java
+++ src/test/java/io/harness/cf/client/api/evaluator/EvaluatorTest.java
@@ -10,7 +10,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
