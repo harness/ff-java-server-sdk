2a5258331cd41ae30f4e8654ad4a9810c996472e
Milos Vasic <milos.vasic@harness.io>
2021-11-10 14:19:54 +0300

    FFM-1543 - Tester base.

--- src/test/java/io/harness/cf/client/api/evaluator/EvaluatorTester.java
+++ src/test/java/io/harness/cf/client/api/evaluator/EvaluatorTester.java
@@ -1,10 +1,18 @@
 package io.harness.cf.client.api.evaluator;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+
+@Slf4j
 class EvaluatorTester implements EvaluatorTesting {
 
     @Override
     public void process(final TestModel data) {
 
+        log.info("Processing test data: START");
+
         // TODO:
+
+        log.info("Processing test data: END");
     }
 }
