cb2def9eb59aeada6b7ff749cb7e38d4f3183391
Milos Vasic <milos.vasic@harness.io>
2021-11-10 13:44:28 +0300

    FFM-1543 - Tests path.

--- src/test/java/io/harness/cf/client/api/evaluator/EvaluatorTest.java
+++ src/test/java/io/harness/cf/client/api/evaluator/EvaluatorTest.java
@@ -1,27 +1,45 @@
 package io.harness.cf.client.api.evaluator;
 
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+@Slf4j
 public class EvaluatorTest {
 
-    private final Gson gson;
     private final List<TestModel> testData;
 
     {
 
-        gson = new Gson();
+
         testData = new LinkedList<>();
     }
 
     @Before
     public void prepareTestData() {
 
-        // TODO: Populate with data
+        try {
+
+            final String testsLocation = "./src/test/ff-test-cases";
+            final String testCasesDirectory = new File(testsLocation).getCanonicalPath();
+
+            log.info(String.format("Test cases directory: %s", testCasesDirectory));
+
+
+            testData.clear();
+            final Gson gson = new Gson();
+
+        } catch (IOException e) {
+
+            Assert.fail(e.getMessage());
+        }
     }
 
     @Test
