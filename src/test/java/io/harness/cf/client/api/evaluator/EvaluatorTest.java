334ebdcb64d274b3a7cb71ac69c289da8dff40e4
Milos Vasic <milos.vasic@harness.io>
2021-11-10 13:30:50 +0300

    FFM-1543 - Evaluator test model.

--- src/test/java/io/harness/cf/client/api/evaluator/EvaluatorTest.java
+++ src/test/java/io/harness/cf/client/api/evaluator/EvaluatorTest.java
@@ -1,12 +1,29 @@
 package io.harness.cf.client.api.evaluator;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class EvaluatorTest {
+
+    private final List<EvaluatorTestModel> testData;
+
+    {
+
+        testData = new LinkedList<>();
+    }
+
+    @Before
+    public void prepareTestData() {
+
+        // TODO: Populate with data
+    }
 
     @Test
     public void testEvaluator() {
 
-
+        // TODO: Process
     }
 }
