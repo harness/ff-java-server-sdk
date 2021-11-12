99890c00dae34ba04595b28aca823be3e7445646
Milos Vasic <milos.vasic@harness.io>
2021-11-12 15:26:33 +0300

    FFM-1543 - Porting the test.

--- src/test/java/io/harness/cf/client/api/EvaluatorTest.java
+++ src/test/java/io/harness/cf/client/api/EvaluatorTest.java
@@ -1,30 +1,117 @@
 package io.harness.cf.client.api;
 
-import static org.testng.Assert.*;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import io.harness.cf.client.api.evaluator.EvaluatorTester;
+import io.harness.cf.client.api.evaluator.EvaluatorTesting;
+import io.harness.cf.client.api.evaluator.TestModel;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-import io.harness.cf.client.dto.Target;
-import java.util.Optional;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.testng.MockitoTestNGListener;
-import org.testng.annotations.Listeners;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Stream;
 
-@Listeners(MockitoTestNGListener.class)
+@Slf4j
 public class EvaluatorTest {
-  private final String identifier = "identifier";
 
-  private final Target target = Target.builder().identifier("harness").build();
+    private final EvaluatorTesting tester;
+    private final List<TestModel> testData;
 
-  @Mock private StorageRepository query;
+    {
 
-  @InjectMocks private Evaluator evaluator;
+        testData = new LinkedList<>();
+        tester = new EvaluatorTester();
+    }
 
-  @org.testng.annotations.Test
-  public void testGetAttrValue() {
+    @Test
+    public void testEvaluator() {
 
-    Optional<Object> attrValue = evaluator.getAttrValue(target, identifier);
+        for (final TestModel model : testData) {
 
-    assertTrue(attrValue.isPresent());
-    assertEquals(attrValue.get(), "harness");
-  }
+            tester.process(model);
+        }
+    }
+
+    @Before
+    public void prepareTestData() {
+
+        try {
+
+            final String testsLocation = "./src/test/ff-test-cases/tests";
+            final String testCasesPath = new File(testsLocation).getCanonicalPath();
+            final File testCasesDirectory = new File(testCasesPath);
+
+            Assert.assertTrue(testCasesDirectory.exists());
+
+            log.info(String.format("Test cases directory: %s", testCasesPath));
+
+            final File[] files = testCasesDirectory.listFiles();
+
+            Assert.assertNotNull(files);
+            Assert.assertTrue(files.length > 0);
+
+            testData.clear();
+
+            final Gson gson = new Gson();
+
+            for (final File file : files) {
+
+                log.info(String.format("Processing the test file: %s", file.getName()));
+
+                Assert.assertTrue(file.getName().toLowerCase(Locale.getDefault()).endsWith(".json"));
+
+                final String json = read(file.getAbsolutePath());
+
+                Assert.assertNotNull(json);
+                Assert.assertFalse(json.trim().isEmpty());
+
+                try {
+
+                    final TestModel model = gson.fromJson(json, TestModel.class);
+
+                    Assert.assertNotNull(model);
+
+                    final String testFile = file.getName();
+                    final String feature = model.flag.getFeature() + testFile;
+
+                    model.testFile = testFile;
+                    model.flag.setFeature(feature);
+
+                    Assert.assertTrue(testData.add(model));
+
+                } catch (JsonSyntaxException e) {
+
+                    Assert.fail(e.getMessage());
+                }
+            }
+        } catch (IOException e) {
+
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    private static String read(final String path) {
+
+        final StringBuilder builder = new StringBuilder();
+
+        try (final Stream<String> stream = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {
+
+            stream.forEach(s -> builder.append(s).append("\n"));
+
+        } catch (IOException e) {
+
+            Assert.fail(e.getMessage());
+        }
+
+        return builder.toString();
+    }
 }
