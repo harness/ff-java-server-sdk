d5f7cdeecb382fe4eb1e25cdcb07f46a2a4a78cf
Milos Vasic <milos.vasic@harness.io>
2021-11-12 14:36:03 +0300

    FFM-1543 - Expected.

--- src/test/java/io/harness/cf/client/api/evaluator/TestModel.java
+++ src/test/java/io/harness/cf/client/api/evaluator/TestModel.java
@@ -3,10 +3,12 @@ package io.harness.cf.client.api.evaluator;
 import io.harness.cf.client.dto.Target;
 import io.harness.cf.model.FeatureConfig;
 
+import java.util.HashMap;
 import java.util.List;
 
 class TestModel {
 
     FeatureConfig flag;
     List<Target> targets;
+    HashMap<String, Boolean> expected;
 }
