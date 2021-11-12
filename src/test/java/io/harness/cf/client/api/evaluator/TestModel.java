099bd21b59089a29ffe7cdbc20d16330f50b1ee7
Milos Vasic <milos.vasic@harness.io>
2021-11-12 14:17:58 +0300

    FFM-1543 - Targets.

--- src/test/java/io/harness/cf/client/api/evaluator/TestModel.java
+++ src/test/java/io/harness/cf/client/api/evaluator/TestModel.java
@@ -1,8 +1,12 @@
 package io.harness.cf.client.api.evaluator;
 
+import io.harness.cf.client.dto.Target;
 import io.harness.cf.model.FeatureConfig;
+
+import java.util.List;
 
 class TestModel {
 
     FeatureConfig flag;
+    List<Target> targets;
 }
