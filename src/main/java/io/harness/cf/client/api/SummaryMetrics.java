4fd5ca8f7aa3a34de76436ae6ba87890adc5bfb1
Enver Bisevac <enver.bisevac@harness.io>
2021-11-15 14:35:22 +0100

    integration test fixed

--- src/main/java/io/harness/cf/client/api/SummaryMetrics.java
+++ src/main/java/io/harness/cf/client/api/SummaryMetrics.java
@@ -1,14 +1,14 @@
 package io.harness.cf.client.api;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
-@Getter
 @Builder
-@EqualsAndHashCode
 @NoArgsConstructor
-@Setter
-@ToString
+@Data
 class SummaryMetrics {
   private String featureName;
   private String variationIdentifier;
--- src/test/java/io/harness/cf/client/api/test/evaluator/EvaluatorTesting.java
+++
@@ -1,6 +0,0 @@
-package io.harness.cf.client.api.test.evaluator;
-
-public interface EvaluatorTesting {
-
-    void process(TestModel data);
-}
