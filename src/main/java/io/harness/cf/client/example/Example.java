6fc06aa899bc8e61ab4119ea1bfbc8de3c6e9911
Milos Vasic <milos85vasic@gmail.com>
2021-11-16 13:53:23 +0300

    FFM-1488 - Example extended and improved.

--- src/main/java/io/harness/cf/client/example/Example.java
+++ src/main/java/io/harness/cf/client/example/Example.java
@@ -6,20 +6,21 @@ import io.harness.cf.client.api.Config;
 import io.harness.cf.client.api.FileMapStore;
 import io.harness.cf.client.dto.Target;
 import java.util.HashMap;
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 class Example {
 
   private static final int capacity;
-  private static final Executor executor;
   private static final HashMap<String, String> keys;
+  private static final ScheduledExecutorService scheduler;
 
   private static final String FREEMIUM_API_KEY = "45d2a13a-c62f-4116-a1a7-86f25d715a2e";
   private static final String NON_FREEMIUM_API_KEY = "9ecc4ced-afc1-45af-9b54-c899cbff4b62";
-  private static final String NON_FREEMIUM_API_KEY_2 = "f48cba87-e4ff-431e-9301-8534cf870ad0";
+  private static final String NON_FREEMIUM_API_KEY_2 = "33e0b6ca-67f0-4af9-921d-945b89e26a3c";
 
   static {
     capacity = 5;
@@ -27,43 +28,45 @@ class Example {
     keys.put("Freemium", FREEMIUM_API_KEY);
     keys.put("Non-Freemium", NON_FREEMIUM_API_KEY);
     keys.put("Non-Freemium-2", NON_FREEMIUM_API_KEY_2);
-    executor = Executors.newScheduledThreadPool(2);
+    scheduler = Executors.newScheduledThreadPool(keys.size());
   }
 
-  @SuppressWarnings("InfiniteLoopStatement")
   public static void main(String... args) {
+
+    Runtime.getRuntime().addShutdownHook(new Thread(scheduler::shutdown));
 
     for (final String keyName : keys.keySet()) {
 
-      executor.execute(
+      final String apiKey = keys.get(keyName);
+      final FileMapStore fileStore = new FileMapStore(keyName);
+      final Client client = new Client(apiKey, Config.builder().store(fileStore).build());
+      final String logPrefix = keyName + " :: " + client.hashCode() + " ";
+
+      Target target =
+          Target.builder()
+              .identifier("target1")
+              .isPrivate(false)
+              .attribute("testKey", "TestValue")
+              .name("target1")
+              .build();
+
+      scheduler.scheduleAtFixedRate(
           () -> {
-            final String apiKey = keys.get(keyName);
-            final FileMapStore fileStore = new FileMapStore(keyName);
-            final Client client = new Client(apiKey, Config.builder().store(fileStore).build());
-            final String logPrefix = keyName + " :: " + client.hashCode() + " ";
+            final boolean bResult = client.boolVariation("flag1", target, false);
+            log.info(logPrefix + "Boolean variation: {}", bResult);
 
-            Target target =
-                Target.builder()
-                    .identifier("target1")
-                    .isPrivate(false)
-                    .attribute("testKey", "TestValue")
-                    .name("target1")
-                    .build();
+            final double dResult = client.numberVariation("flag2", target, -1);
+            log.info(logPrefix + "Number variation: {}", dResult);
 
-            while (true) {
-              final boolean bResult = client.boolVariation("flag1", target, false);
-              log.info(logPrefix + "Boolean variation: {}", bResult);
+            final String sResult = client.stringVariation("flag3", target, "NO_VALUE!!!");
+            log.info(logPrefix + "String variation: {}", sResult);
 
-              final double dResult = client.numberVariation("flag2", target, -1);
-              log.info(logPrefix + "Number variation: {}", dResult);
-
-              final String sResult = client.stringVariation("flag3", target, "NO_VALUE!!!");
-              log.info(logPrefix + "String variation: {}", sResult);
-
-              final JsonObject jResult = client.jsonVariation("flag4", target, new JsonObject());
-              log.info(logPrefix + "JSON variation: {}", jResult);
-            }
-          });
+            final JsonObject jResult = client.jsonVariation("flag4", target, new JsonObject());
+            log.info(logPrefix + "JSON variation: {}", jResult);
+          },
+          0,
+          10,
+          TimeUnit.SECONDS);
     }
 
     Thread.yield();
