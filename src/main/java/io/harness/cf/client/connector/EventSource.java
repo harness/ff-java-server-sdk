f7a7290b3f16cf4887d7ab59915a570619b1d25b
Enver Bisevac <enver.bisevac@harness.io>
2022-04-28 10:30:47 +0200

    fixed logging in sse

--- src/main/java/io/harness/cf/client/connector/EventSource.java
+++ src/main/java/io/harness/cf/client/connector/EventSource.java
@@ -6,9 +6,12 @@ import com.here.oksse.ServerSentEvent;
 import io.harness.cf.client.dto.Message;
 import io.harness.cf.client.logger.LogUtil;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -31,21 +34,39 @@ public class EventSource implements ServerSentEvent.Listener, AutoCloseable, Ser
 
   public EventSource(@NonNull String url, Map<String, String> headers, @NonNull Updater updater) {
     this.updater = updater;
-    okSse = new OkSse();
-
-    if (log.isDebugEnabled()) {
-      loggingInterceptor = new HttpLoggingInterceptor();
-      loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-      okSse.getClient().interceptors().add(loggingInterceptor);
-    } else {
-      okSse.getClient().interceptors().remove(loggingInterceptor);
-      loggingInterceptor = null;
-    }
-
+    okSse = new OkSse(makeStreamClient());
     builder = new Request.Builder().url(url);
+    headers.put("User-Agent", "JavaSDK " + io.harness.cf.Version.VERSION);
     headers.forEach(builder::header);
     updater.onReady();
     log.info("EventSource initialized with url {} and headers {}", url, headers);
+  }
+
+  protected OkHttpClient makeStreamClient() {
+    OkHttpClient.Builder httpClientBuilder =
+        new OkHttpClient.Builder().readTimeout(0L, TimeUnit.SECONDS).retryOnConnectionFailure(true);
+    if (log.isDebugEnabled()) {
+      loggingInterceptor = new HttpLoggingInterceptor();
+      loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+      httpClientBuilder.addInterceptor(loggingInterceptor);
+    } else {
+      httpClientBuilder.interceptors().remove(loggingInterceptor);
+      loggingInterceptor = null;
+    }
+    httpClientBuilder.addInterceptor(
+        chain -> {
+          final Request request =
+              chain
+                  .request()
+                  .newBuilder()
+                  .addHeader("X-Request-ID", UUID.randomUUID().toString())
+                  .build();
+          log.info("interceptor: requesting url {}", request.url().url());
+
+          return chain.proceed(request);
+        });
+    log.info("stream http client definition complete");
+    return httpClientBuilder.build();
   }
 
   @Override
