4cd0d647774705333cd169805f31b3de66134f10
Milos Vasic <milos.vasic@harness.io>
2022-05-12 13:21:08 +0300

    [FFM-3215] Stress-test SDK multi-threading - First touches.

---
+++ examples/src/main/java/io/harness/ff/examples/StressTest.java
@@ -0,0 +1,16 @@
+package io.harness.ff.examples;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * This application stress-test the SDK
+ * with aggressive multi-threading execution.
+ */
+@Slf4j
+public class StressTest {
+
+    public static void main(String... args) {
+
+
+    }
+}
