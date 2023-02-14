f491d8552ab3c020f676e991a729a315b17fc295
Clint Checketts <clintchecketts@churchofjesuschrist.org>
2023-02-14 15:33:08 -0700

    Add broader exception handling in case authentication fails
    Also only wait for 5 seconds instead of forever, allowing the failure check to proppogate

--- src/main/java/io/harness/cf/client/connector/ConnectorException.java
+++ src/main/java/io/harness/cf/client/connector/ConnectorException.java
@@ -3,18 +3,33 @@ package io.harness.cf.client.connector;
 public class ConnectorException extends Exception {
 
   private final int httpCode;
+  private final boolean shouldRetry;
+
   private final String httpReason;
 
   public ConnectorException(String message) {
     super(message);
     this.httpCode = 0;
     this.httpReason = "";
+    this.shouldRetry = true;
   }
 
   public ConnectorException(String message, int httpCode, String httpMsg) {
     super(message);
     this.httpCode = httpCode;
     this.httpReason = httpMsg;
+    this.shouldRetry = true;
+  }
+  
+    public ConnectorException(String message, boolean shouldRetry, Exception e) {
+    super(message, e);
+    this.shouldRetry = shouldRetry;
+    this.httpCode = 0;
+    this.httpReason = "";
+  }
+
+  public boolean shouldRetry() {
+    return shouldRetry;
   }
 
   @Override
