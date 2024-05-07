69539cf8f2bf58f6a0e9bfedc4184b314552ca1c
Andrew Bell <115623869+andybharness@users.noreply.github.com>
2024-05-07 14:13:49 +0100

    fix tests

--- src/test/java/io/harness/cf/client/api/dispatchers/Endpoints.java
+++ src/test/java/io/harness/cf/client/api/dispatchers/Endpoints.java
@@ -6,7 +6,7 @@ public class Endpoints {
   public static final String FEATURES_ENDPOINT =
       "/api/1.0/client/env/00000000-0000-0000-0000-000000000000/feature-configs?cluster=1";
   public static final String SEGMENTS_ENDPOINT =
-      "/api/1.0/client/env/00000000-0000-0000-0000-000000000000/target-segments?cluster=1";
+      "/api/1.0/client/env/00000000-0000-0000-0000-000000000000/target-segments?cluster=1&rules=v2";
   public static final String STREAM_ENDPOINT = "/api/1.0/stream?cluster=1";
   public static final String SIMPLE_BOOL_FLAG_ENDPOINT =
       "/api/1.0/client/env/00000000-0000-0000-0000-000000000000/feature-configs/simplebool?cluster=1";
