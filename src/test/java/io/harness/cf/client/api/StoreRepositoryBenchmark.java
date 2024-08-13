74babcbb0991e2801cfb21138683576761da2e39
Arek Szostak <arkadiusz.szostak@harness.io>
2024-08-13 09:32:34 +0100

    Further twicking

--- src/test/java/io/harness/cf/client/api/StoreRepositoryBenchmark.java
+++ src/test/java/io/harness/cf/client/api/StoreRepositoryBenchmark.java
@@ -3,6 +3,7 @@ package io.harness.cf.client.api;
 import com.google.gson.Gson;
 import io.harness.cf.model.FeatureConfig;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.*;
 
 /*
@@ -23,9 +24,15 @@ public class StoreRepositoryBenchmark {
   private List<FeatureConfig> featureConfigs;
   private List<FeatureConfig> updatedFeatureConfigs;
   private final Gson gson = new Gson();
+  private final int FeatureConfigSize = 10000;
+  private final int CacheSize = FeatureConfigSize * 2;
 
   @Setup
   public void setup() throws Exception {
+    TestUtils tu = new TestUtils();
+    featureConfigs = tu.CreateBenchmarkData(FeatureConfigSize, 1);
+    updatedFeatureConfigs = tu.CreateBenchmarkData(FeatureConfigSize, 2);
+
     setupRepoWithCurrentOnlyRepository();
     setupRepoWithCurrentAndPreviousRepository();
   }
@@ -34,23 +41,7 @@ public class StoreRepositoryBenchmark {
   public void setupRepoWithCurrentOnlyRepository() throws Exception {
 
     TestUtils tu = new TestUtils();
-    currentOnlyRepo = new StorageRepository(new CaffeineCache(10000), null, false);
-
-    // Define the sample size for benchmarking
-    int sampleSize = 1000;
-
-    // Create benchmark data
-    featureConfigs = tu.CreateBenchmarkData(sampleSize, 1);
-    updatedFeatureConfigs = tu.CreateBenchmarkData(sampleSize, 2);
-
-    // Ensure data is created correctly
-    if (featureConfigs == null || featureConfigs.size() != sampleSize) {
-      throw new IllegalStateException("Feature configs setup failed.");
-    }
-    if (updatedFeatureConfigs == null || updatedFeatureConfigs.size() != sampleSize) {
-      throw new IllegalStateException("Updated feature configs setup failed.");
-    }
-
+    currentOnlyRepo = new StorageRepository(new CaffeineCache(CacheSize), null, false);
     // Initial loading of feature configurations
     loadFlags(currentOnlyRepo, featureConfigs);
   }
@@ -59,41 +50,28 @@ public class StoreRepositoryBenchmark {
   public void setupRepoWithCurrentAndPreviousRepository() throws Exception {
 
     TestUtils tu = new TestUtils();
-    previousAndCurrentRepo = new StorageRepository(new CaffeineCache(10000), null, true);
-
-    // Define the sample size for benchmarking
-    int sampleSize = 1000;
-
-    // Create benchmark data
-    featureConfigs = tu.CreateBenchmarkData(sampleSize, 1);
-    updatedFeatureConfigs = tu.CreateBenchmarkData(sampleSize, 2);
-
-    // Ensure data is created correctly
-    if (featureConfigs == null || featureConfigs.size() != sampleSize) {
-      throw new IllegalStateException("Feature configs setup failed.");
-    }
-    if (updatedFeatureConfigs == null || updatedFeatureConfigs.size() != sampleSize) {
-      throw new IllegalStateException("Updated feature configs setup failed.");
-    }
-
+    previousAndCurrentRepo = new StorageRepository(new CaffeineCache(CacheSize), null, true);
     // Initial loading of feature configurations
     loadFlags(previousAndCurrentRepo, featureConfigs);
   }
 
   @Fork(value = 1, warmups = 1)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
   @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
   public void BenchmarkLoadFeatureConfigCurrentOnly() {
-    // Measure time taken to load the updated feature configurations while storing only current
-    // snapshot
+    // Measure average time taken to load the updated feature configurations while storing only
+    // current snapshot
     loadFlags(currentOnlyRepo, updatedFeatureConfigs);
   }
 
-  
   @Fork(value = 1, warmups = 1)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
   @Benchmark
+  @BenchmarkMode(Mode.AverageTime)
   public void BenchmarkLoadFeatureConfigPreviousAndCurrent() {
-    // Measure time taken to load the updated feature configurations while storing only current
-    // snapshot as well as keeping previous snapshot.
+    // Measure average time taken to load the updated feature configurations while storing current
+    // config as well as keeping previous one.
     loadFlags(previousAndCurrentRepo, updatedFeatureConfigs);
   }
 
